cf81f4a5bc4b86c4a0086aad8750f0e348104e76
Andrei Yankavets <andreyyankovez@gmail.com>
2025-01-28 18:10:41 +0300

    [ADD] Create mapper library

---
+++ ecommerce-util/src/main/java/by/innowise/exception/UnsupportedMapMethodException.java
@@ -0,0 +1,9 @@
+package by.innowise.exception;
+
+public class UnsupportedMapMethodException extends RuntimeException {
+
+    public UnsupportedMapMethodException(String message) {
+        super(message);
+    }
+
+}

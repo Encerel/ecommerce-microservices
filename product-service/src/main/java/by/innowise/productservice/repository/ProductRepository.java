078ac56ccfbb14228443eddbec74e93f020b00ab
Andrei Yankavets <andreyyankovez@gmail.com>
2025-01-28 18:04:31 +0300

    [ADD] Create product repository

---
+++ product-service/src/main/java/by/innowise/productservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package by.innowise.productservice.repository;
+
+import by.innowise.productservice.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}

c4db5c305cdc372950d6312aef59715c1d809974
Andrei Yankavets <andreyyankovez@gmail.com>
2025-01-28 18:09:15 +0300

    [ADD] Create product controller

---
+++ product-service/src/main/java/by/innowise/productservice/web/controller/ProductController.java
@@ -0,0 +1,41 @@
+package by.innowise.productservice.web.controller;
+
+
+import by.innowise.productservice.model.dto.ProductCreateDto;
+import by.innowise.productservice.model.dto.ProductReadDto;
+import by.innowise.productservice.service.ProductService;
+import by.innowise.productservice.web.payload.ServerResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+
+    @GetMapping
+    public List<ProductReadDto> getProducts() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ProductReadDto getProduct(@PathVariable Integer id) {
+        return productService.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<ServerResponse> createProduct(@RequestBody ProductCreateDto product) {
+        return productService.save(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ServerResponse> deleteProduct(@PathVariable Integer id) {
+        return productService.delete(id);
+    }
+}

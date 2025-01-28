badcc62ddaffb66878196d645b8545d285612dd1
Andrei Yankavets <andreyyankovez@gmail.com>
2025-01-28 18:09:34 +0300

    [ADD] Create Message Util class

---
+++ product-service/src/main/java/by/innowise/productservice/constant/Message.java
@@ -0,0 +1,10 @@
+package by.innowise.productservice.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Message {
+
+    public static final String PRODUCT_SAVED_SUCCESSFULLY = "Product saved successfully";
+    public static final String PRODUCT_DELETED_SUCCESSFULLY = "Product deleted successfully";
+}

8e26dd9a512e0724a1645b888eac8657767c78f4
Andrei Yankavets <andreyyankovez@gmail.com>
2025-01-31 19:23:47 +0300

    [ADD] Add validation

--- order-service/src/main/java/by/innowise/orderservice/model/dto/OrderCreateDto.java
+++ order-service/src/main/java/by/innowise/orderservice/model/dto/OrderCreateDto.java
@@ -1,12 +1,14 @@
 package by.innowise.orderservice.model.dto;
 
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -14,6 +16,8 @@ import java.util.List;
 @Builder
 public class OrderCreateDto {
 
-    private String userId;
-    private List<ProductReadDto> products;
+    private UUID userId;
+
+    @NotEmpty(message = "Order must contain at least one item")
+    private List<OrderItemCreateDto> items;
 }

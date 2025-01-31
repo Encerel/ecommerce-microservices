ec7d56fb0e78a19b26e75838407360cde9699442
Andrei Yankavets <andreyyankovez@gmail.com>
2025-01-31 19:02:15 +0300

    [REMOVE] Remove unnecessary files

--- order-service/src/main/java/by/innowise/orderservice/model/entity/product/ProductStatus.java
+++
@@ -1,6 +0,0 @@
-package by.innowise.orderservice.model.entity.product;
-
-public enum ProductStatus {
-    AVAILABLE,
-    OUT_OF_STOCK,
-}
--- order-service/src/main/java/by/innowise/orderservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package by.innowise.orderservice.repository;
-
-import by.innowise.orderservice.model.entity.product.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}

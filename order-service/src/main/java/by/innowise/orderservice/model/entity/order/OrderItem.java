2618ad3240232fadb62d26ca90b5d2c386287396
Andrei Yankavets <andreyyankovez@gmail.com>
2025-01-31 15:22:26 +0300

    [ADD] Create order item entity

---
+++ order-service/src/main/java/by/innowise/orderservice/model/entity/order/OrderItem.java
@@ -0,0 +1,35 @@
+package by.innowise.orderservice.model.entity.order;
+
+import by.innowise.orderservice.model.entity.product.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders_products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+
+}

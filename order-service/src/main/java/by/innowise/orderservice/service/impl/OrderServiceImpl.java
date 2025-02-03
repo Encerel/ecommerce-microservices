d802db1daa3589e9148ebb27f0814244cde69e87
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-03 12:38:13 +0300

    [ADD] Add inventory check before order placing

--- order-service/src/main/java/by/innowise/orderservice/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/by/innowise/orderservice/service/impl/OrderServiceImpl.java
@@ -1,8 +1,10 @@
 package by.innowise.orderservice.service.impl;
 
+import by.innowise.orderservice.exception.NotEnoughProductsInStockException;
 import by.innowise.orderservice.mapper.OrderItemCreateMapper;
 import by.innowise.orderservice.mapper.OrderReadMapper;
 import by.innowise.orderservice.model.dto.OrderCreateDto;
+import by.innowise.orderservice.model.dto.OrderItemCreateDto;
 import by.innowise.orderservice.model.dto.OrderItemReadDto;
 import by.innowise.orderservice.model.dto.OrderReadDto;
 import by.innowise.orderservice.model.entity.Order;
@@ -10,6 +12,7 @@ import by.innowise.orderservice.model.entity.OrderItem;
 import by.innowise.orderservice.model.entity.OrderStatus;
 import by.innowise.orderservice.repository.OrderRepository;
 import by.innowise.orderservice.service.OrderService;
+import by.innowise.orderservice.web.client.InventoryClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -28,12 +32,29 @@ public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
     private final OrderItemCreateMapper orderItemCreateMapper;
     private final OrderReadMapper orderReadMapper;
+    private final InventoryClient inventoryClient;
 
     @Override
     @Transactional
     public OrderReadDto placeOrder(OrderCreateDto order) {
 
         log.info("Placing order for user with id {}", order.getUserId());
+
+        log.debug("Getting ids of products");
+        List<Integer> productIds = order.getItems().stream()
+                .map(OrderItemCreateDto::getProductId)
+                .toList();
+        log.debug("Trying to check availability of products in stock");
+        Map<Integer, Boolean> inventoryStatus = inventoryClient.checkInventory(productIds);
+        List<Integer> missingProducts = inventoryStatus.entrySet().stream()
+                .filter(item -> !item.getValue())
+                .map(Map.Entry::getKey)
+                .toList();
+
+        if (!missingProducts.isEmpty()) {
+            log.warn("Not enough products in stock {}", missingProducts);
+            throw new NotEnoughProductsInStockException(missingProducts);
+        }
 
         Order orderForSave = Order.builder()
                 .orderDate(LocalDate.now())

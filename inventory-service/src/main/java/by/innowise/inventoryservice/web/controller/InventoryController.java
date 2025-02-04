4a0270e3413aaaa53db6d62112228948f15d6ebd
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-04 18:15:32 +0300

    [ADD] Create returnProductsToInventory endpoint

--- inventory-service/src/main/java/by/innowise/inventoryservice/web/controller/InventoryController.java
+++ inventory-service/src/main/java/by/innowise/inventoryservice/web/controller/InventoryController.java
@@ -3,6 +3,7 @@ package by.innowise.inventoryservice.web.controller;
 import by.innowise.inventoryservice.model.api.Product;
 import by.innowise.inventoryservice.model.api.ProductQuantity;
 import by.innowise.inventoryservice.service.InventoryService;
+import by.innowise.inventoryservice.web.payload.ServerResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,5 +24,10 @@ public class InventoryController {
     @PostMapping("/take")
     public List<Product> takeProductsFromInventory(@RequestBody List<ProductQuantity> products) {
         return inventoryService.takeProductsFromInventory(products);
+    }
+
+    @PostMapping("/return")
+    public ServerResponse returnProductsToInventory(@RequestBody List<ProductQuantity> products) {
+        return inventoryService.returnProductsToInventory(products);
     }
 }

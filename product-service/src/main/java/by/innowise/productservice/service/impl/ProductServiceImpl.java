00b2ad051762d567bd8f946c9cb9574e5b001cb2
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-04 15:24:35 +0300

    [ADD] Implement updateStatus method and add check when product is saving

--- product-service/src/main/java/by/innowise/productservice/service/impl/ProductServiceImpl.java
+++ product-service/src/main/java/by/innowise/productservice/service/impl/ProductServiceImpl.java
@@ -1,11 +1,13 @@
 package by.innowise.productservice.service.impl;
 
 
+import by.innowise.productservice.exception.ProductAlreadyExistsException;
 import by.innowise.productservice.exception.ProductNotFoundException;
 import by.innowise.productservice.mapper.ProductCreateMapper;
 import by.innowise.productservice.mapper.ProductReadMapper;
 import by.innowise.productservice.model.dto.ProductCreateDto;
 import by.innowise.productservice.model.dto.ProductReadDto;
+import by.innowise.productservice.model.dto.ProductStatusRequest;
 import by.innowise.productservice.model.dto.ProductsBatchReadDto;
 import by.innowise.productservice.model.entity.Product;
 import by.innowise.productservice.repository.ProductRepository;
@@ -14,6 +16,7 @@ import by.innowise.productservice.web.payload.ServerResponse;
 import by.innowise.productservice.web.payload.response.AdviceErrorMessage;
 import by.innowise.productservice.web.payload.response.MessageServerResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -23,12 +26,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static by.innowise.productservice.constant.Message.PRODUCT_DELETED_SUCCESSFULLY;
-import static by.innowise.productservice.constant.Message.PRODUCT_SAVED_SUCCESSFULLY;
+import static by.innowise.productservice.constant.ErrorMessage.PRODUCT_NOT_FOUND;
+import static by.innowise.productservice.constant.Message.*;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
@@ -43,9 +47,11 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductReadDto findById(Integer id) {
+        log.info("Try to find product with id: {}", id);
         return productReadMapper.toDto(productRepository.findById(id)
                 .orElseThrow(
-                        () -> new ProductNotFoundException(id))
+                        () -> new ProductNotFoundException(id)
+                )
         );
     }
 
@@ -59,7 +65,7 @@ public class ProductServiceImpl implements ProductService {
                 products.add(productReadMapper.toDto(foundProduct.get()));
             } else {
                 errors.add(AdviceErrorMessage.builder()
-                        .message(String.format("Product with id %d not found!", id))
+                        .message(String.format(PRODUCT_NOT_FOUND, id))
                         .status(HttpStatus.NOT_FOUND.value())
                         .build());
             }
@@ -73,9 +79,40 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     @Transactional
+    public ResponseEntity<ServerResponse> updateStatus(Integer productId, ProductStatusRequest status) {
+        log.info("Try to update status for product with id: {}", productId);
+        Optional<Product> foundProduct = productRepository.findById(productId);
+        if (foundProduct.isEmpty()) {
+            log.warn("Product with id {} not found!", productId);
+            throw new ProductNotFoundException(productId);
+        }
+        Product product = foundProduct.get();
+        product.setStatus(status.getProductStatus());
+        productRepository.save(product);
+        log.info("Status of product with id {} updated successfully!", productId);
+
+        ServerResponse serverResponse = MessageServerResponse.builder()
+                .message(PRODUCT_STATUS_UPDATED_SUCCESSFULLY)
+                .status(HttpStatus.OK.value())
+                .build();
+        return ResponseEntity.ok(serverResponse);
+    }
+
+    @Override
+    @Transactional
     public ResponseEntity<ServerResponse> save(ProductCreateDto productCreateDto) {
+        log.info("Try to find product with name: {} in db", productCreateDto.getName());
+        Optional<Product> foundByName = productRepository.findByName(productCreateDto.getName());
+
+        if (foundByName.isPresent()) {
+            log.warn("Product with name {} already exists!", productCreateDto.getName());
+            throw new ProductAlreadyExistsException(productCreateDto.getName());
+        }
+
+        log.debug("Map productCreateDto in entity!");
         Product product = productCreateMapper.toEntity(productCreateDto);
         productRepository.save(product);
+        log.info("Product with name {} saved!", productCreateDto.getName());
         ServerResponse message = MessageServerResponse.builder()
                 .message(PRODUCT_SAVED_SUCCESSFULLY)
                 .build();

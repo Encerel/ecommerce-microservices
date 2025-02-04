5298722f0158c4c3e669612ef5db8e47ae7edfb3
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-04 15:09:34 +0300

    [Add] Add validation under fields

--- order-service/src/main/java/by/innowise/orderservice/model/dto/OrderItemCreateDto.java
+++ order-service/src/main/java/by/innowise/orderservice/model/dto/OrderItemCreateDto.java
@@ -1,5 +1,7 @@
 package by.innowise.orderservice.model.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +13,10 @@ import lombok.NoArgsConstructor;
 @Builder
 public class OrderItemCreateDto {
 
+    @NotNull(message = "Product id should not be null!")
     private Integer productId;
+
+    @Size(min = 0, message = "Quantity should be more than 0!")
     private Integer quantity;
 
 }

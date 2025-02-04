37dc562d33e91196472078d026d4165784a23aa4
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-04 18:18:43 +0300

    [ADD] Implement getOrderById, updateOrderStatus and cancelOrder methods

--- order-service/src/main/java/by/innowise/orderservice/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/by/innowise/orderservice/service/impl/OrderServiceImpl.java
@@ -1,8 +1,13 @@
 package by.innowise.orderservice.service.impl;
 
+import by.innowise.orderservice.exception.OrderAlreadyCanceledException;
+import by.innowise.orderservice.exception.OrderNotFoundException;
+import by.innowise.orderservice.exception.ProductNotFoundException;
 import by.innowise.orderservice.mapper.OrderItemCreateMapper;
 import by.innowise.orderservice.mapper.OrderReadMapper;
 import by.innowise.orderservice.model.api.Product;
+import by.innowise.orderservice.model.api.ProductQuantity;
+import by.innowise.orderservice.model.api.ProductsBatch;
 import by.innowise.orderservice.model.dto.OrderCreateDto;
 import by.innowise.orderservice.model.dto.OrderItemCreateDto;
 import by.innowise.orderservice.model.dto.OrderItemReadDto;
@@ -13,14 +18,18 @@ import by.innowise.orderservice.model.entity.OrderStatus;
 import by.innowise.orderservice.repository.OrderRepository;
 import by.innowise.orderservice.service.OrderService;
 import by.innowise.orderservice.web.client.InventoryClient;
+import by.innowise.orderservice.web.client.ProductClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import static java.util.stream.Collectors.joining;
 
 @Service
 @Transactional(readOnly = true)
@@ -32,6 +41,7 @@ public class OrderServiceImpl implements OrderService {
     private final OrderItemCreateMapper orderItemCreateMapper;
     private final OrderReadMapper orderReadMapper;
     private final InventoryClient inventoryClient;
+    private final ProductClient productClient;
 
     @Override
     @Transactional
@@ -66,7 +76,15 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public OrderReadDto getOrderById(Integer orderId) {
-        return null;
+
+        Order order = orderRepository.findById(orderId).orElseThrow(
+                () -> new OrderNotFoundException(orderId)
+        );
+
+        List<Integer> productsIds = findProductsIds(order.getItems());
+        List<Product> productsByIds = findProductsByIds(productsIds);
+
+        return orderReadMapper.toDto(order, productsByIds);
     }
 
     @Override
@@ -75,13 +93,31 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
+    @Transactional
     public OrderReadDto updateOrderStatus(Integer orderId, OrderStatus status) {
-        return null;
+
+        Order order = orderRepository.findById(orderId).orElseThrow(
+                () -> new OrderNotFoundException(orderId)
+        );
+
+        List<Integer> productsIds = findProductsIds(order.getItems());
+        List<Product> productsByIds = findProductsByIds(productsIds);
+
+        order.setStatus(status);
+        return orderReadMapper.toDto(orderRepository.save(order), productsByIds);
     }
 
     @Override
+    @Transactional
     public OrderReadDto cancelOrder(Integer orderId) {
-        return null;
+        Order order = orderRepository.findById(orderId).orElseThrow(
+                () -> new OrderNotFoundException(orderId)
+        );
+        if (order.getStatus() == OrderStatus.CANCELED) {
+            throw new OrderAlreadyCanceledException(orderId);
+        }
+        inventoryClient.returnProductsToInventory(map(order.getItems()));
+        return updateOrderStatus(orderId, OrderStatus.CANCELED);
     }
 
     @Override
@@ -93,5 +129,36 @@ public class OrderServiceImpl implements OrderService {
     public boolean checkProductAvailability(List<OrderItemReadDto> items) {
         return false;
     }
+
+    private List<Integer> findProductsIds(List<OrderItem> items) {
+        return items.stream()
+                .map(OrderItem::getProductId)
+                .toList();
+    }
+
+    private List<Product> findProductsByIds(List<Integer> productsIds) {
+        ProductsBatch batchResponse = productClient.findByIds(productsIds);
+        if (!batchResponse.getErrors().isEmpty()) {
+            String errorMessage = batchResponse.getErrors().stream()
+                    .map(error -> error.getMessage() + "\n")
+                    .collect(joining());
+            throw new ProductNotFoundException(errorMessage);
+        }
+        return batchResponse.getProducts();
+    }
+
+    private List<ProductQuantity> map(List<OrderItem> orderItems) {
+        List<ProductQuantity> result = new ArrayList<>();
+        for (OrderItem orderItem : orderItems) {
+            ProductQuantity request = ProductQuantity.builder()
+                    .productId(orderItem.getProductId())
+                    .quantity(orderItem.getQuantity())
+                    .build();
+            result.add(request);
+        }
+        return result;
+    }
+
+
 
 }

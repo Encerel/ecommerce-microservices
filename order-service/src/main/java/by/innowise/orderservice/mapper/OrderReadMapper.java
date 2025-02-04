7b44a5238146ff38a9316bf216178a38d749c1a8
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-04 15:12:47 +0300

    [ADD] Add total price calculation

--- order-service/src/main/java/by/innowise/orderservice/mapper/OrderReadMapper.java
+++ order-service/src/main/java/by/innowise/orderservice/mapper/OrderReadMapper.java
@@ -1,12 +1,16 @@
 package by.innowise.orderservice.mapper;
 
 import by.innowise.mapper.Mapper;
+import by.innowise.orderservice.model.api.Product;
+import by.innowise.orderservice.model.dto.OrderItemReadDto;
 import by.innowise.orderservice.model.dto.OrderReadDto;
 import by.innowise.orderservice.model.entity.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -14,17 +18,19 @@ public class OrderReadMapper implements Mapper<Order, OrderReadDto> {
 
     private final OrderItemReadMapper orderItemReadMapper;
 
-    @Override
-    public OrderReadDto toDto(Order entity) {
+    public OrderReadDto toDto(Order entity, List<Product> products) {
         if (entity == null) {
             return null;
         }
+        List<OrderItemReadDto> orderItems = orderItemReadMapper.toListDto(entity.getItems(), products);
+        BigDecimal totalPrice = calculateTotalPrice(orderItems);
         return OrderReadDto.builder()
                 .id(entity.getId())
                 .userId(entity.getUserId())
-                .items(orderItemReadMapper.toListDto(entity.getItems()))
+                .items(orderItems)
                 .status(entity.getStatus())
                 .orderDate(entity.getOrderDate())
+                .totalPrice(totalPrice)
                 .build();
     }
 
@@ -40,5 +46,13 @@ public class OrderReadMapper implements Mapper<Order, OrderReadDto> {
                 .status(dto.getStatus())
                 .orderDate(LocalDate.now())
                 .build();
+    }
+
+    private BigDecimal calculateTotalPrice(List<OrderItemReadDto> items) {
+        BigDecimal totalPrice = BigDecimal.valueOf(0);
+        for (OrderItemReadDto item : items) {
+            totalPrice = totalPrice.add(BigDecimal.valueOf(item.getQuantity()).multiply(BigDecimal.valueOf(item.getProductPrice())));
+        }
+        return totalPrice;
     }
 }

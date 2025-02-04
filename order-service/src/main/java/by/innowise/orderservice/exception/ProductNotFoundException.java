01d17670db1aa88ccc8eec024626e18a8091372a
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-04 15:20:23 +0300

    [REFACTOR] Remove unused constructor

--- order-service/src/main/java/by/innowise/orderservice/exception/ProductNotFoundException.java
+++ order-service/src/main/java/by/innowise/orderservice/exception/ProductNotFoundException.java
@@ -2,9 +2,7 @@ package by.innowise.orderservice.exception;
 
 public class ProductNotFoundException extends RuntimeException {
 
-    private static final String MESSAGE = "Product with id %d not found";
-
-    public ProductNotFoundException(Integer id) {
-        super(String.format(MESSAGE, id));
+    public ProductNotFoundException(String message) {
+        super(message);
     }
 }

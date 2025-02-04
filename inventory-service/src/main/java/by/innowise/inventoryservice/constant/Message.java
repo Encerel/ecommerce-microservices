98a04c7200f2dc23d23b90d70000371b998b5d52
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-04 18:09:12 +0300

    [ADD] Create Message util class

---
+++ inventory-service/src/main/java/by/innowise/inventoryservice/constant/Message.java
@@ -0,0 +1,9 @@
+package by.innowise.inventoryservice.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Message {
+
+    public static final String PRODUCTS_RETURNED_SUCCESSFULLY = "Products returned successfully";
+}

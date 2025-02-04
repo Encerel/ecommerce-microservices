55fde539ae3926c2562679db770f8494c4fcdb4d
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-04 14:40:53 +0300

    [ADD] Create constant ErrorMessage

---
+++ order-service/src/main/java/by/innowise/orderservice/constant/ErrorMessage.java
@@ -0,0 +1,11 @@
+package by.innowise.orderservice.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ErrorMessage {
+
+    public static final String UNKNOWN_RESPONSE_TYPE = "Unknown response type";
+    public static final String ERROR_PARSING_RESPONSE = "Error parsing response";
+    public static final String NOT_ENOUGH_ITEMS_IN_STOCK = "Not enough items in stock!";
+}

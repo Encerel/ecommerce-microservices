7343e03477244734970faa378360c664516ef415
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-04 15:08:51 +0300

    [Add] Add placeOrder endpoint

--- order-service/src/main/java/by/innowise/orderservice/web/controller/OrderController.java
+++ order-service/src/main/java/by/innowise/orderservice/web/controller/OrderController.java
@@ -1,11 +1,26 @@
 package by.innowise.orderservice.web.controller;
 
+import by.innowise.orderservice.model.dto.OrderCreateDto;
+import by.innowise.orderservice.model.dto.OrderReadDto;
+import by.innowise.orderservice.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/orders")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    public OrderReadDto placeOrder(@RequestBody @Valid OrderCreateDto orderCreateDto) {
+        return orderService.placeOrder(orderCreateDto);
+    }
 
 
 }

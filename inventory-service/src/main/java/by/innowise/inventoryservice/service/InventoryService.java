8e7d2635aa19b5091f6cadc05c1c29a9109f763c
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-04 18:15:51 +0300

    [ADD] Add returnProductsToInventory method

--- inventory-service/src/main/java/by/innowise/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/by/innowise/inventoryservice/service/InventoryService.java
@@ -3,6 +3,7 @@ package by.innowise.inventoryservice.service;
 
 import by.innowise.inventoryservice.model.api.Product;
 import by.innowise.inventoryservice.model.api.ProductQuantity;
+import by.innowise.inventoryservice.web.payload.ServerResponse;
 
 import java.util.List;
 
@@ -10,4 +11,5 @@ public interface InventoryService {
 
     List<Product> takeProductsFromInventory(List<ProductQuantity> products);
 
+    ServerResponse returnProductsToInventory(List<ProductQuantity> products);
 }

8e72e9929140e7deb72eaee730b90adcb48c5ab7
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-05 13:59:58 +0300

    [ADD] Add findAllOrders, findOrderById, findOrdersByUser

--- order-service/src/main/java/by/innowise/orderservice/web/controller/OrderController.java
+++ order-service/src/main/java/by/innowise/orderservice/web/controller/OrderController.java
@@ -1,11 +1,16 @@
 package by.innowise.orderservice.web.controller;
 
 import by.innowise.orderservice.model.dto.OrderCreateDto;
-import by.innowise.orderservice.model.dto.OrderReadDto;
+import by.innowise.orderservice.model.dto.OrderDetailsDto;
+import by.innowise.orderservice.model.dto.OrderSummaryDto;
 import by.innowise.orderservice.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -15,14 +20,30 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    public OrderReadDto placeOrder(@RequestBody @Valid OrderCreateDto orderCreateDto) {
+    public OrderDetailsDto placeOrder(@RequestBody @Valid OrderCreateDto orderCreateDto) {
         return orderService.placeOrder(orderCreateDto);
     }
 
     @PostMapping("/cancel/{orderId}")
-    public OrderReadDto cancelOrder(@PathVariable Integer orderId) {
+    public OrderDetailsDto cancelOrder(@PathVariable Integer orderId) {
         return orderService.cancelOrder(orderId);
     }
 
+
+    @GetMapping
+    public Page<OrderSummaryDto> findAllOrders(@RequestParam(defaultValue = "0") Integer offset,
+                                               @RequestParam(defaultValue = "5") Integer pageSize) {
+        return orderService.findAll(offset, pageSize);
+    }
+
+    @GetMapping("/{orderId}")
+    public OrderDetailsDto findOrderById(@PathVariable Integer orderId) {
+        return orderService.findById(orderId);
+    }
+
+    @GetMapping("/user/{userId}")
+    public List<OrderSummaryDto> findOrdersByUserId(@PathVariable UUID userId) {
+        return orderService.findAllByUserId(userId);
+    }
 
 }

b0f05b0f1720a63605a00265383caa33296757c6
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-06 15:09:47 +0300

    [ADD] Add new exceptions support

--- inventory-service/src/main/java/by/innowise/inventoryservice/web/controller/advice/InventoryControllerAdvice.java
+++ inventory-service/src/main/java/by/innowise/inventoryservice/web/controller/advice/InventoryControllerAdvice.java
@@ -1,14 +1,17 @@
 package by.innowise.inventoryservice.web.controller.advice;
 
-import by.innowise.inventoryservice.exception.ProductNotFoundException;
-import by.innowise.inventoryservice.exception.ProductOutStockException;
+import by.innowise.inventoryservice.exception.*;
 import by.innowise.inventoryservice.web.payload.ServerResponse;
 import by.innowise.inventoryservice.web.payload.response.AdviceErrorMessage;
 import by.innowise.inventoryservice.web.payload.response.OutStockProductResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static by.innowise.inventoryservice.constant.ErrorMessage.NOT_ENOUGH_ITEMS_IN_STOCK;
 
@@ -35,4 +38,41 @@ public class InventoryControllerAdvice {
         );
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(InventoryNotFoundException.class)
+    public ResponseEntity<ServerResponse> handleInventoryNotFoundException(InventoryNotFoundException e) {
+        ServerResponse response = new AdviceErrorMessage(
+                e.getMessage(),
+                HttpStatus.BAD_REQUEST.value()
+        );
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(UnknownResponseException.class)
+    public ResponseEntity<ServerResponse> handleUnknownResponseException(UnknownResponseException exception) {
+        ServerResponse serverResponse = AdviceErrorMessage.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .build();
+        return new ResponseEntity<>(serverResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ErrorParsingResponseException.class)
+    public ResponseEntity<ServerResponse> handleErrorParsingResponseException(ErrorParsingResponseException exception) {
+        ServerResponse serverResponse = AdviceErrorMessage.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .build();
+        return new ResponseEntity<>(serverResponse, HttpStatus.BAD_REQUEST);
+    }
+
 }

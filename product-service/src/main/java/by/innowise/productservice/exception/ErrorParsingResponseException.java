06a650e6f3858c46501e8c6a75873afb08326435
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-06 14:56:35 +0300

    [ADD] Create ErrorParsingResponseException

---
+++ product-service/src/main/java/by/innowise/productservice/exception/ErrorParsingResponseException.java
@@ -0,0 +1,9 @@
+package by.innowise.productservice.exception;
+
+public class ErrorParsingResponseException extends RuntimeException {
+
+    public ErrorParsingResponseException(String message) {
+        super(message);
+    }
+
+}

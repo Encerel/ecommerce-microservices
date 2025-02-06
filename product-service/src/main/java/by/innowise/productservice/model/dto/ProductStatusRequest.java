8b683d6b15c677d698b0becc251c87e82c3f1903
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-06 15:45:11 +0300

    [ADD] Add validation

--- product-service/src/main/java/by/innowise/productservice/model/dto/ProductStatusRequest.java
+++ product-service/src/main/java/by/innowise/productservice/model/dto/ProductStatusRequest.java
@@ -1,6 +1,7 @@
 package by.innowise.productservice.model.dto;
 
 import by.innowise.productservice.model.entity.ProductStatus;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,6 +9,7 @@ import lombok.Data;
 @AllArgsConstructor
 public class ProductStatusRequest {
 
+    @NotNull(message = "Product status must not be null")
     private ProductStatus productStatus;
 
 }

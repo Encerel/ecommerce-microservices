e170117f21c4cc2c99070f8d470cea344aba1adc
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-06 14:58:21 +0300

    [ADD] Add new messages

--- inventory-service/src/main/java/by/innowise/inventoryservice/constant/ErrorMessage.java
+++ inventory-service/src/main/java/by/innowise/inventoryservice/constant/ErrorMessage.java
@@ -5,4 +5,6 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class ErrorMessage {
     public static final String NOT_ENOUGH_ITEMS_IN_STOCK = "Not enough items in stock!";
+    public static final String UNKNOWN_RESPONSE_TYPE = "Unknown response type";
+    public static final String ERROR_PARSING_RESPONSE = "Error parsing response";
 }
--- product-service/src/main/java/by/innowise/productservice/constant/ErrorMessage.java
+++ product-service/src/main/java/by/innowise/productservice/constant/ErrorMessage.java
@@ -5,5 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class ErrorMessage {
 
+    public static final String UNKNOWN_RESPONSE_TYPE = "Unknown response type";
+    public static final String ERROR_PARSING_RESPONSE = "Error parsing response";
     public static final String PRODUCT_NOT_FOUND = "Product with id %d not found!";
 }

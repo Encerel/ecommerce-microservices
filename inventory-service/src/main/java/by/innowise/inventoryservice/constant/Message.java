56a98446fef0c4ba68b71cf848eba09b8ccc3c6f
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-06 15:01:08 +0300

    [ADD] Add new messages

--- inventory-service/src/main/java/by/innowise/inventoryservice/constant/Message.java
+++ inventory-service/src/main/java/by/innowise/inventoryservice/constant/Message.java
@@ -6,4 +6,6 @@ import lombok.experimental.UtilityClass;
 public class Message {
 
     public static final String PRODUCTS_RETURNED_SUCCESSFULLY = "Products returned successfully";
+    public static final String PRODUCTS_DELETED_SUCCESSFULLY = "Products deleted successfully";
+    public static final String PRODUCTS_ADDED_IN_INVENTORY_SUCCESSFULLY = "Products added in inventory successfully";
 }
--- product-service/src/main/java/by/innowise/productservice/constant/Message.java
+++ product-service/src/main/java/by/innowise/productservice/constant/Message.java
@@ -5,7 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Message {
 
-    public static final String PRODUCT_SAVED_SUCCESSFULLY = "Product saved successfully";
-    public static final String PRODUCT_DELETED_SUCCESSFULLY = "Product deleted successfully";
-    public static final String PRODUCT_STATUS_UPDATED_SUCCESSFULLY = "Product status updated successfully";
+    public static final String PRODUCT_SAVED_SUCCESSFULLY = "Product saved successfully!";
+    public static final String PRODUCT_DELETED_SUCCESSFULLY = "Product deleted successfully!";
+    public static final String PRODUCT_STATUS_UPDATED_SUCCESSFULLY = "Product status updated successfully!";
 }

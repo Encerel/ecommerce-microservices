d31b204288c8d7cf36dee6b852c6f70a5cf01ac5
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-06 14:56:53 +0300

    [ADD] Create UnknownResponseException

---
+++ order-service/src/main/java/by/innowise/orderservice/exception/UnknownResponseException.java
@@ -0,0 +1,7 @@
+package by.innowise.orderservice.exception;
+
+public class UnknownResponseException extends RuntimeException {
+    public UnknownResponseException(String message) {
+        super(message);
+    }
+}

c0200ff5755d3f1e8e18650d7f598405b60ee704
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-06 15:11:31 +0300

    [ADD] Add addNewProductInInventory,increaseProductStock and deleteByProductId methods

--- inventory-service/src/main/java/by/innowise/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/by/innowise/inventoryservice/service/InventoryService.java
@@ -1,15 +1,22 @@
 package by.innowise.inventoryservice.service;
 
 
-import by.innowise.inventoryservice.model.api.Product;
+import by.innowise.inventoryservice.model.api.OrderItem;
 import by.innowise.inventoryservice.model.api.ProductQuantity;
+import by.innowise.inventoryservice.model.api.ProductStock;
 import by.innowise.inventoryservice.web.payload.ServerResponse;
 
 import java.util.List;
 
 public interface InventoryService {
 
-    List<Product> takeProductsFromInventory(List<ProductQuantity> products);
+    List<OrderItem> takeProductsFromInventory(List<ProductQuantity> products);
 
     ServerResponse returnProductsToInventory(List<ProductQuantity> products);
+
+    ServerResponse addNewProductInInventory(ProductQuantity productQuantity);
+
+    ProductStock increaseProductStock(ProductQuantity productQuantity);
+
+    ServerResponse deleteByProductId(Integer productId);
 }

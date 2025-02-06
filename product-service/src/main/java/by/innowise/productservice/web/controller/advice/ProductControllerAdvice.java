93dd091e41e95ad40604146f91bf5c7c3326c5cf
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-06 15:42:14 +0300

    [Add] Add new exception support

--- product-service/src/main/java/by/innowise/productservice/web/controller/advice/ProductControllerAdvice.java
+++ product-service/src/main/java/by/innowise/productservice/web/controller/advice/ProductControllerAdvice.java
@@ -1,12 +1,16 @@
 package by.innowise.productservice.web.controller.advice;
 
-import by.innowise.productservice.exception.ProductNotFoundException;
+import by.innowise.productservice.exception.*;
 import by.innowise.productservice.web.payload.ServerResponse;
 import by.innowise.productservice.web.payload.response.AdviceErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ProductControllerAdvice {
@@ -15,8 +19,62 @@ public class ProductControllerAdvice {
     public ResponseEntity<ServerResponse> handleNotFoundUserException(ProductNotFoundException exception) {
         ServerResponse serverResponse = AdviceErrorMessage.builder()
                 .message(exception.getMessage())
-                .status(HttpStatus.NOT_FOUND.value())
+                .status(HttpStatus.BAD_REQUEST.value())
                 .build();
-        return new ResponseEntity<>(serverResponse, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(serverResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ProductAlreadyExistsException.class)
+    public ResponseEntity<ServerResponse> handleProductAlreadyExistsException(ProductAlreadyExistsException exception) {
+        ServerResponse serverResponse = AdviceErrorMessage.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.CONFLICT.value())
+                .build();
+        return new ResponseEntity<>(serverResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(InventoryNotFoundException.class)
+    public ResponseEntity<ServerResponse> handleNotFoundInventoryException(InventoryNotFoundException exception) {
+        ServerResponse serverResponse = AdviceErrorMessage.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .build();
+        return new ResponseEntity<>(serverResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ProductInStockException.class)
+    public ResponseEntity<ServerResponse> handleProductInStockException(ProductInStockException exception) {
+        ServerResponse serverResponse = AdviceErrorMessage.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .build();
+        return new ResponseEntity<>(serverResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UnknownResponseException.class)
+    public ResponseEntity<ServerResponse> handleUnknownResponseException(UnknownResponseException exception) {
+        ServerResponse serverResponse = AdviceErrorMessage.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .build();
+        return new ResponseEntity<>(serverResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ErrorParsingResponseException.class)
+    public ResponseEntity<ServerResponse> handleErrorParsingResponseException(ErrorParsingResponseException exception) {
+        ServerResponse serverResponse = AdviceErrorMessage.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .build();
+        return new ResponseEntity<>(serverResponse, HttpStatus.BAD_REQUEST);
     }
 }

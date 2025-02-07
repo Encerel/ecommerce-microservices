e0bc1bd55c8bddc09d98b115ebca595b8bd57a45
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-07 12:58:19 +0300

    [ADD] Create WebClientConfiguration

---
+++ inventory-service/src/main/java/by/innowise/inventoryservice/config/WebClientConfig.java
@@ -0,0 +1,25 @@
+package by.innowise.inventoryservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${microservices.api.product}")
+    private String productServiceUrl;
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient productWebClient(WebClient.Builder builder) {
+        return builder.baseUrl(productServiceUrl).build();
+    }
+}
---
+++ product-service/src/main/java/by/innowise/productservice/config/WebClientConfig.java
@@ -0,0 +1,27 @@
+package by.innowise.productservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${microservices.api.inventory}")
+    private String inventoryServiceUrl;
+
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient inventoryWebClient(WebClient.Builder builder) {
+        return builder.baseUrl(inventoryServiceUrl).build();
+    }
+
+}

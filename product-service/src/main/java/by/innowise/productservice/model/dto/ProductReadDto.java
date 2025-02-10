aa06f5e8f7c9bfdf538230bfe14295d6a735b978
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-10 20:29:08 +0300

    [ADD] Add product validation

--- product-service/src/main/java/by/innowise/productservice/model/dto/ProductReadDto.java
+++ product-service/src/main/java/by/innowise/productservice/model/dto/ProductReadDto.java
@@ -1,6 +1,9 @@
 package by.innowise.productservice.model.dto;
 
 import by.innowise.productservice.model.entity.ProductStatus;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,10 +15,20 @@ import lombok.NoArgsConstructor;
 @Builder
 public class ProductReadDto {
 
+    @NotNull(message = "Product id must be not null!")
+    @Positive(message = "Product id must be over than zero")
     private Integer id;
+
+    @NotEmpty(message = "Product name must not be null or empty!")
     private String name;
+
+    @NotEmpty(message = "Product description must not be null or empty!")
     private String description;
+
+    @Positive(message = "Product price must be over than zero")
     private Double price;
+
+    @NotEmpty(message = "Product status must not be null or empty!")
     private ProductStatus status;
 
 }

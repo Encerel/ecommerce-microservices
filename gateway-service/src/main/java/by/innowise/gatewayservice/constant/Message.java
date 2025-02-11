bcb3cda5ab78961cfebaefddccfb0273b4a533da
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-11 17:56:45 +0300

    [ADD] Create message utility class

---
+++ gateway-service/src/main/java/by/innowise/gatewayservice/constant/Message.java
@@ -0,0 +1,10 @@
+package by.innowise.gatewayservice.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Message {
+
+    public static final String RATE_LIMIT_EXCEEDED_PLEASE_TRY_AGAIN_LATER = "Rate limit exceeded. Please try again later.";
+
+}

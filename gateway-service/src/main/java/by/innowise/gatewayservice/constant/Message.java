830ea256e31e76734950eade1e01acddf40ebbff
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-12 19:06:01 +0300

    [ADD] Add new message

--- gateway-service/src/main/java/by/innowise/gatewayservice/constant/Message.java
+++ gateway-service/src/main/java/by/innowise/gatewayservice/constant/Message.java
@@ -6,5 +6,5 @@ import lombok.experimental.UtilityClass;
 public class Message {
 
     public static final String RATE_LIMIT_EXCEEDED_PLEASE_TRY_AGAIN_LATER = "Rate limit exceeded. Please try again later.";
-
+    public static final String SERVICE_IS_TEMPORARILY_UNAVAILABLE_PLEASE_TRY_AGAIN_LATER = "Service is temporarily unavailable. Please try again later.";
 }

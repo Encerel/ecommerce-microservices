d0df59b49d835a9d9e79f70b455c4cd42b562518
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-13 14:52:07 +0300

    [INIT] Init notification-service

---
+++ notification-service/src/test/java/by/innowise/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.innowise.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

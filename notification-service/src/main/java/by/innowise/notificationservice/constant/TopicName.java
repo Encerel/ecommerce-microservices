d1a77687d90ca9e0347a3c0e96f5453df7c8204d
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-14 15:44:54 +0300

    [ADD] Create TopicName

---
+++ notification-service/src/main/java/by/innowise/notificationservice/constant/TopicName.java
@@ -0,0 +1,12 @@
+package by.innowise.notificationservice.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TopicName {
+
+    public static final String ORDER_CREATE_EVENTS_TOPIC = "order-create-events-topic";
+    public static final String ORDER_STATUS_UPDATES_EVENTS_TOPIC = "order-status-updates-events-topic";
+    public static final String PRODUCT_CREATE_EVENTS_TOPIC = "product-create-events-topic";
+    public static final String PRODUCT_STATUS_UPDATES_EVENTS_TOPIC = "product-status-updates-topic";
+}
---
+++ product-service/src/main/java/by/innowise/productservice/constant/TopicName.java
@@ -0,0 +1,10 @@
+package by.innowise.productservice.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TopicName {
+
+    public static final String PRODUCT_CREATE_EVENTS_TOPIC = "product-create-events-topic";
+    public static final String PRODUCT_STATUS_UPDATES_EVENTS_TOPIC = "product-status-updates-topic";
+}

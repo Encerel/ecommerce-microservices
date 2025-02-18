a37c209e4c3644a1da49fb02aec1854b02989097
Andrei Yankavets <andreyyankovez@gmail.com>
2025-02-18 16:04:30 +0300

    [REFACTOR] Change exception name

--- product-service/src/main/java/by/innowise/productservice/exception/InventoryNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package by.innowise.productservice.exception;
-
-public class InventoryNotFoundException extends RuntimeException {
-
-    public InventoryNotFoundException(String message) {
-        super(message);
-    }
-}
